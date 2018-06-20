ea13516cc6620fa0516a3956293a8d04ee20e3ca
NgocQuynh1206 <36333470+NgocQuynh1206@users.noreply.github.com>
2018-06-20 13:46:53 +0700

    Update LeftMenuComponent.js

--- src/layout/components/body/leftLayout/LeftMenuComponent.js
+++ src/layout/components/body/leftLayout/LeftMenuComponent.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import {Layout, Input} from 'antd';
+import {Layout} from 'antd';
 const {Sider} = Layout;
-const {Search} = Input;
 export default class LeftMenu extends React.Component {
 	render () 
 	{
 		return (
 			<Sider width={200} style={{ background: '#fff' }}>
-				<Search
-			      placeholder="Search"
-			      onSearch={value => console.log(value)}
-			      enterButton
-			      style={{marginTop: '4%', marginLeft: '2.5%', width: '95%'}}
-			    />
+				
 			</Sider>
 		)
 	}
